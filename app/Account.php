295b04090ee1b3dedbf4ef5e0ba5f3ecc30bd2fc
sherifkhalil <eng_sherifkhalil@hotmail.com>
2016-06-11 10:50:10 +0200

    Admin Panel

--- app/Account.php
+++ app/Account.php
@@ -5,10 +5,14 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use App\User;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Account extends Model
 {
-    public function user()
-    {
-        return $this->belongsTo('App\User');
-    }
+	protected $fillable = [
+        'name', 'email', 'id_number', 'certificate', 'type',
+    ];
+
+	use SoftDeletes;
+    protected $dates = ['deleted_at'];
 }
